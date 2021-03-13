e23bb8ff1733069e99287a4e74aa80948a04e169
MatthiasBakken <matthias.bakken@gmail.com>
2021-03-12 22:28:30 -0800

    finished project

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 
 // A- finding across the entire DOM
 const header = document.getElementsByTagName("header");
+const nav = document.querySelector("#mainNav");
 const logoTitle = document.getElementById("logoTitle");
 const firstCard = document.getElementsByClassName("card")[0];
 // B- finding within one particular element
@@ -24,32 +25,45 @@ const link2FirstCard = firstCard.querySelectorAll(".card-link")[1];
 const anchorTags = document.querySelectorAll(".menu-item");
 anchorTags.forEach(tag => console.log(tag.innerHTML));
 let anchorTagsArray = []
-anchorTags.forEach(tag => anchorTagsArray.push(tag.href))
-console.log(anchorTagsArray)
+anchorTags.forEach(tag => anchorTagsArray.push({title: tag.innerHTML, link: tag.href}))
+console.log(anchorTagsArray);
+console.log(anchorTagsArray.filter(tag => tag.title === "Home"));
 
 // 👉 3- Changing an element's text content
 //  A- Change the cat-related content into dog-related content
 //  B- Have the students research online the difference between textContent and innerText
-
+imageFirstCard.src = "https://preview.redd.it/zu3bpvldcsf41.jpg?width=640&crop=smart&auto=webp&s=37464c4ef6f97176f76e6a45375c0086adea0059";
+titleFirstCard.innerHTML = "Dog";
+subtitleFirstCard.innerHTML = "Looks like a dog";
+textFirstCard.innerHTML = "Smells like a dog, must be a dog!";
+link2FirstCard.innerHTML = "Dog Ipsum";
 
 // 👉 4- Changing any property
 //  A- Using dot notation to change a few attributes
 //  B- Using .setAttribute to change a few attributes
-
+titleFirstCard.setAttribute("class", "extra-stuff");
 
 // 👉 5- Changing the styling of an element
 //  A- By changing the class names on the element with the classList API
 //  B- By manipulating inline styles on the element
-
+titleFirstCard.classList.toggle("card-title", true);
 
 // 👉 6- Creating new elements from scratch and appending them
 // Create a new link inside the nav for "Blog"
-
-
+const node = document.createElement("A");
+node.classList.add("menu-item");
+node.innerHTML = "Blog";
+node.href = "#";
+console.log(node);
+console.log(nav);
+nav.appendChild(node);
+console.log(nav);
 // 👉 7- Making a copy of the card and appending it to the card group
 // DOM nodes can only exist in one spot in the DOM
 // We cannot append the same copy multiple times
-
+const cardCopy = document.querySelector(".card");
+console.log(cardCopy);
+document.querySelector(".card-group").appendChild(cardCopy);
 
 // 👉 8- Removing an existing element and putting it back [STRETCH if time allows]
 
